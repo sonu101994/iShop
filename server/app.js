bbcb625be266e5e6082ddec6b514a7de04faf072
sonu101994 <sonu10211995@gmail.com>
2026-05-27 01:34:10 +0530

    cors reset

--- server/app.js
+++ server/app.js
@@ -34,26 +34,16 @@ app.use(express.static(path.join(__dirname,"public")));
 
 // cors setup
 
-const allowedOrigins = [
-    "http://localhost:3000",
-    process.env.CLIENT_URL,
-].filter(Boolean);
+const cors = require("cors");
 
 app.use(
     cors({
-        origin: function (origin, callback) {
-            if (!origin || allowedOrigins.includes(origin)) {
-                callback(null, true);
-            } else {
-                callback(new Error("Not allowed by CORS"));
-            }
-        },
+        origin: true,
         credentials: true,
         methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
         allowedHeaders: ["Content-Type", "Authorization"],
     })
 );
-
 // testing api
 app.get("/", (req, res) => {
     res.json({
